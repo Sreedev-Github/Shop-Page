f5372a43fe7f2a44a318883fd05c9db91d31dda0
Sreedev-Github <sreedev0612@gmail.com>
2024-03-27 16:49:05 +0530

    Completed listing items, category, also filtering items with category.

---
+++ data.js
@@ -0,0 +1,98 @@
+export const products = [
+    {
+        product_image: 'https://www.superherotoystore.com/cdn/shop/products/AAAK2AABY157-5_5000x.jpg?v=1679381149',
+        product_name: "BATMAN 7",
+        price: "₹ 4,799.00",
+        category: "Action Figure",
+        age:  ["Adults", "Kids"]
+    },
+
+    {
+        product_image: 'https://www.superherotoystore.com/cdn/shop/files/AACD348AABY137-1_5000x.jpg?v=1685532485',
+        product_name: "BATMOBILE",
+        price: "₹ 9,999.00",
+        category: "Car",
+        age:  ["Adults", "Kids"]
+    },
+    
+    {
+        product_image: 'https://www.superherotoystore.com/cdn/shop/products/1100021040079_2_5000x.jpg?v=1600848694',
+        product_name: "JUSTICE LEAGUE",
+        price: "₹ 9,999.00",
+        category: "Action Figure",
+        age:  ["Kids"]
+    },
+    
+    {
+        product_image: 'https://www.superherotoystore.com/cdn/shop/files/1_da80f447-d5ee-4b77-8da2-60809d03a3f5_5000x.jpg?v=1706532827',
+        product_name: "THE DARK KNIGHT",
+        price: "₹ 2,799.00",
+        category: "Action Figure",
+        age:  ["Adults", "Kids"]
+    },
+
+    {
+        product_image: 'https://www.superherotoystore.com/cdn/shop/files/6_3b5d7a8e-c45d-46cc-9937-393ce0bbb563_5000x.jpg?v=1698927160',
+        product_name: "RED BULL RB19",
+        price: "₹ 7,999.00",
+        category: "Car",
+        age:  ["Adults", "Kids"]
+    },
+
+    {
+        product_image: 'https://www.superherotoystore.com/cdn/shop/files/Untitleddesign_331f7494-0de5-45f7-8adb-33a2a1b58f26_5000x.jpg?v=1689571249',
+        product_name: "TOMICA SUBARU",
+        price: "₹ 2,999.00",
+        category: "Car",
+        age:  ["Kids"]
+    },
+    
+    {
+        product_image: 'https://www.superherotoystore.com/cdn/shop/products/AAEK0202AACG0108-1_5000x.jpg?v=1624797354',
+        product_name: "TINTIN - DESERT JEEP",
+        price: "₹ 4,999.00",
+        category: "Car",
+        age:  ["Kids"]
+    },
+    
+    {
+        product_image: 'https://www.superherotoystore.com/cdn/shop/files/1_df753d8b-f980-4cf9-9bb4-a059bf65514c_5000x.jpg?v=1708593965',
+        product_name: "SUZUKI HAYABUSA",
+        price: "₹ 5,999.00",
+        category: "Bike",
+        age:  ["Kids", "Adults"]
+    },
+   
+    {
+        product_image: 'https://www.superherotoystore.com/cdn/shop/files/1_7f3e24bc-818e-45a1-b341-07daede850f2_5000x.jpg?v=1698834245',
+        product_name: "KAWASAKI NINJA H2",
+        price: "₹ 3,999.00",
+        category: "Bike",
+        age:  ["Kids"]
+    },
+    
+    {
+        product_image: 'https://www.superherotoystore.com/cdn/shop/files/1_7648ec8c-04a2-4470-a715-68544be4a066_5000x.jpg?v=1698840509',
+        product_name: "BMW R 1250",
+        price: "₹ 4,999.00",
+        category: "Bike",
+        age:  ["Kids", "Adults"]
+    },
+    
+    {
+        product_image: 'https://www.superherotoystore.com/cdn/shop/products/AAHQ818AAGE2327_1_5000x.jpg?v=1704788221',
+        product_name: "RICK & MORTY MUG",
+        price: "₹ 449.00",
+        category: "Accessories",
+        age:  ["Kids", "Adults"]
+    },
+
+    {
+        product_image: 'https://www.superherotoystore.com/cdn/shop/products/AAHQ818AAGE2333_5000x.jpg?v=1704796726',
+        product_name: "HUG POSTER",
+        price: "₹ 299.00",
+        category: "Accessories",
+        age:  ["Kids", "Adults"]
+    },
+
+]
--- script.js
+++ script.js
@@ -1,29 +1,143 @@
-const accordion = document.querySelectorAll('.accordion')
-console.log(accordion.length);
+import { products } from "./data.js";
 
-// IIFE - to make sure the accordions are opened up when the page loads
-(function(){
-    accordion.forEach((acc)=>{
-        const accordionData = acc.nextElementSibling
-        if (!accordionData.style.maxHeight){
-            accordionData.style.maxHeight = accordionData.scrollHeight + "px";
-        }
-    })
-})()
-
+const accordion = document.querySelectorAll(".accordion");
+const cardContainer = document.querySelector(".cards-container");
+const productCat = document.querySelector(".product-category");
 
 for (let i = 0; i < accordion.length; i++) {
-    accordion[i].addEventListener('click', function(e){
-        // We use this. isntead of e.target.value as if we click on the icon it has no sibling so the e.target.value will return null and the accordion won't work,
-        // So instead we are using this. to make sure it is targetting the acccordion itslef
-        // So intead of the event this function is always called by the accordion itself this referes to the accordion which called it
-        this.classList.toggle("active-accordion");
-        const accordionData = this.nextElementSibling;
-            if (accordionData.style.maxHeight) {
-                accordionData.style.maxHeight = null;
-            } else {
-                accordionData.style.maxHeight = accordionData.scrollHeight + "px";
-            }
-        
-    });
+  accordion[i].addEventListener("click", function (e) {
+    // We use this. isntead of e.target.value as if we click on the icon it has no sibling so the e.target.value will return null and the accordion won't work,
+    // So instead we are using this. to make sure it is targetting the acccordion itslef
+    // So intead of the event this function is always called by the accordion itself this referes to the accordion which called it
+    this.classList.toggle("active-accordion");
+    const accordionData = this.nextElementSibling;
+    if (accordionData.style.maxHeight) {
+      accordionData.style.maxHeight = null;
+    } else {
+      accordionData.style.maxHeight = accordionData.scrollHeight + "px";
+    }
+  });
+}
+
+const displayProducts = function (data) {
+  cardContainer.innerHTML = data
+    .map((product) => {
+      return `
+        <div class="card">
+        <div class="card-image">
+              <img src="${product.product_image}" alt="Joker">
+          </div>
+          <div class="card-text">
+              <p class="card-title">${product.product_name}</p>
+              <p class="card-title">${product.price}</p>
+          </div>
+          <button class="addToCart">Add to Cart</button>
+          </div>
+        `;
+    })
+    .join("");
+};
+
+displayProducts(products);
+
+// Categories
+
+const setCategory = () => {
+  const allCats = products.map((item) => item.category);
+  const categories = [
+    "All",
+    ...allCats.filter((item, i) => {
+      return allCats.indexOf(item) === i;
+    }),
+  ];
+
+  productCat.innerHTML = categories
+    .map((cat) => {
+      return `
+    <div class="options">
+    <input type="checkbox" class="cat-input" name="${cat}" id="${cat}">
+    <label for="${cat}">${cat}</label>
+    <span class="category-value">99</span>
+    </div>
+    `;
+    })
+    .join("");
+
+  //We can just call the event inside the parent container to avoid using for each loop for every category
+  const allCatsInputs = document.querySelectorAll(".cat-input");
+  let selectedCategories = [];
+  
+  allCatsInputs.forEach((input) => {
+      input.addEventListener("click", function (e) {
+          if (this.checked) {
+                selectedCategories.push(this.name);
+          } else {
+              selectedCategories = selectedCategories.filter(cat => cat !== this.name);
+          }
+          filterProducts();
+      });
+  });
+  const filterProducts = () => {
+    if (selectedCategories.length === 0) {
+        displayProducts(products);
+    }else if (selectedCategories.includes("All")){
+        displayProducts(products);
+    }
+     else {
+        const filteredProducts = products.filter((item) => selectedCategories.includes(item.category));
+        displayProducts(filteredProducts);
+    }
+};
+};
+
+setCategory();
+
+
+
+// IIFE - to make sure the accordions are opened up when the page loads
+(function () {
+  accordion.forEach((acc) => {
+    const accordionData = acc.nextElementSibling;
+    if (!accordionData.style.maxHeight) {
+      accordionData.style.maxHeight = accordionData.scrollHeight + "px";
+    }
+  });
+})();
+
+{
+  /*
+ categories.forEach((cat)=>{
+    const productCat = document.querySelector('.product-category')
+    const newCat = document.createElement('div')
+    newCat.classList = 'options'
+    newCat.innerHTML = `
+        <input type="checkbox" name="${cat}" id="${cat}">
+        <label for="${cat}">${cat}</label>
+        <span class="category-value">99</span>
+    `
+    productCat.appendChild(newCat)
+    console.log(cat);
+   })
+
+const displayProducts = function (data) {
+
+    const newCard = document.createElement("div");
+    newCard.classList = "card";
+    newCard.innerHTML = `
+          <div class="card-image">
+              <img src="${products[i].product_image}" alt="Joker">
+          </div>
+          <div class="card-text">
+              <p class="card-title">${products[i].product_name}</p>
+              <p class="card-title">${products[i].price}</p>
+          </div>
+          <button class="addToCart">Add to Cart</button>
+          `;
+
+    cardContainer.appendChild(newCard);
+};
+
+
+
+*/
 }
